29d791fe6f9cdd29d76aba3aeaf5f7cb583c1cc8
sergey.abrosimov <sergey.abrosimov@bk.ru>
2024-12-18 15:27:29 +0300

    Test changed

--- src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -21,7 +21,7 @@ public class CardDeliveryTest {
     public void validDataAndSuccessfulSubmissionForm() {
         Selenide.open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Воронеж");
-        String planningDate = generateDate(4, "dd.mm.yyyy");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Абросимов Сергей");
