b731c2b4f8e43986e5d83067f5bcf5930b28f5ec
sergey.abrosimov <sergey.abrosimov@bk.ru>
2024-12-18 18:33:55 +0300

    Test changed

--- src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -13,21 +13,21 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class CardDeliveryTest {
 
-    private String generateDate(long addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    private String generateDate() {
+        return LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Test
     public void validDataAndSuccessfulSubmissionForm() {
         Selenide.open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Воронеж");
-        String planningDate = generateDate(4, "dd.MM.yyyy");
+        String planningDate = generateDate();
         $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Абросимов Сергей");
         $("[data-test-id='phone'] input").setValue("+79991234567");
-        $("[data-test-id='agreemen']").click();
-        $("[data-test-id='.button']").click();
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
         $(".notification_content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate));
